2ade2b303ecd1522fda2dfc344fdb19f024dd31d
Aleksandar <Aleksandar@DESKTOP-VK7T1RV.home>
2020-10-21 19:57:29 +0200

    Oppgave 1-3

--- src/no/hvl/dat100/jplab11/common/TODO.java
+++ src/no/hvl/dat100/jplab11/common/TODO.java
@@ -1,5 +1,10 @@
 package no.hvl.dat100.jplab11.common;
 
+import java.util.Arrays;
+
+import no.hvl.dat100.jplab11.oppgave2.Tekst;
+import no.hvl.dat100.jplab11.oppgave3.Blogg;
+
 public class TODO {
 
 	public static String method() {
@@ -15,5 +20,28 @@ public class TODO {
 	   return "Konstruktøren for klassen " + className + " er ikke implementert";
 		
 	}
+	
+	// main-metode for testing
+	/*
+	public static void main(String[] args) {
+		
+		// Test av oppgave 3m, search()
+		Blogg samling = new Blogg(5);
+		
+		Tekst tekst1 = new Tekst(11,"Ole Olsen", "23-10-2019","en tekst");
+		Tekst tekst2 = new Tekst(72,"Ole Olsen", "23-10-2019","blablabla");
+		Tekst tekst3 = new Tekst(298,"Ole Olsen", "23-10-2019","qfjpqof");
+		Tekst tekst4 = new Tekst(721,"Ole Olsen", "23-10-2019","pojwpqf");
+		Tekst tekst5 = new Tekst(35,"Ole Olsen", "23-10-2019","test blatest");
+		
+		samling.leggTil(tekst1);
+		samling.leggTil(tekst2);
+		samling.leggTil(tekst3);
+		samling.leggTil(tekst4);
+		samling.leggTil(tekst5);
+		
+		int[] test = samling.search("bla");
+		System.out.println(Arrays.toString(test));
+	}*/
 
 }
